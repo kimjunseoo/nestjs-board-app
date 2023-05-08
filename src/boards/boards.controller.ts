082b3370d07dad62629f5bc9b11c925ea59423d7
kimjunseoo <zz0905k@naver.com>
2023-05-08 20:40:27 +0900

    feat : board dto

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get , Post, Body } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -15,9 +16,8 @@ export class BoardsController {
     
     @Post()
     createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
+        @Body() createBoardDto: CreateBoardDto
         ): Board{
-        return this.boardsService.createBoard(title, description);
+        return this.boardsService.createBoard(createBoardDto);
     }
 }
